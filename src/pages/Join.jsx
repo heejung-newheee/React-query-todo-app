e634524e9f668306fcc58c3853782d38c616257a
heejung-newheee <aldls2311@gmail.com>
2023-07-14 15:35:53 +0900

    Feat: 회원가입/로그인/로그아웃

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -9,13 +9,36 @@ import { StCloseBtn, StModalName, StLabel, StInputForm, StTextArea, StPtag } fro
 import { StButton, StButtonWrap } from 'components/Button';
 import { StTodoBoard } from 'components/Todo/TodoList';
 import { useNavigate } from 'react-router-dom';
+import { useMutation, useQueryClient } from 'react-query';
+import { register } from 'api/users';
+import shortid from 'shortid';
 
 function Join() {
-    const [id, onChangeIdHandler] = useInput('');
+    const [uid, onChangeIdHandler] = useInput('');
     const [pw, onChangePwHandler] = useInput('');
     const [pwConfirm, onChangePwConfirmHandler] = useInput('');
     const [name, onChangeNameHandler] = useInput('');
     const navigate = useNavigate();
+
+    // 쿼리관련
+    const queryClient = useQueryClient();
+    const mutationJoin = useMutation(register, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('users');
+        }
+    });
+    const registerHandler = (e) => {
+        e.preventDefault();
+        const newUser = {
+            id: shortid.generate(),
+            uid: uid,
+            pw: pw,
+            userName: name,
+            isLogin: false
+        };
+        mutationJoin.mutate(newUser);
+        navigate('/login');
+    };
 
     return (
         <StBg>
@@ -25,10 +48,10 @@ function Join() {
                     <StCloseBtn>
                         <StModalName>회원 가입</StModalName>
                     </StCloseBtn>
-                    <form>
+                    <form onSubmit={registerHandler}>
                         <StLabel htmlFor="">이름</StLabel>
                         <StInputForm placeholder="아이디를 입력하세요" name="" type="text" value={name} onChange={onChangeNameHandler} />
-                        {id.length < 2 ? (
+                        {name.length < 2 ? (
                             <StPtag>2자 이상 입력해주세요</StPtag>
                         ) : (
                             <StPtag>
@@ -36,8 +59,8 @@ function Join() {
                             </StPtag>
                         )}
                         <StLabel htmlFor="">아이디</StLabel>
-                        <StInputForm placeholder="아이디를 입력하세요" name="" type="text" value={id} onChange={onChangeIdHandler} />
-                        {id.length < 5 ? (
+                        <StInputForm placeholder="아이디를 입력하세요" name="" type="text" value={uid} onChange={onChangeIdHandler} />
+                        {uid.length < 5 ? (
                             <StPtag>5자 이상 입력해주세요</StPtag>
                         ) : (
                             <StPtag>
@@ -63,14 +86,14 @@ function Join() {
                             <StPtag>비밀번호가 같은지 확인하세요</StPtag>
                         )}
                         <StButtonWrap>
-                            <StButton disabled={id.length >= 5 && pw.length >= 5 ? false : true} type="submit" $fontColor={'black'}>
+                            <StButton disabled={uid.length >= 5 && pw.length >= 5 ? false : true} type="submit" $fontColor={'black'}>
                                 회원 가입
                             </StButton>
                             <StButton
                                 onClick={() => {
                                     navigate('/');
                                 }}
-                                type="submit"
+                                type="button"
                                 $fontColor={'black'}>
                                 홈으로가기
                             </StButton>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,11 +8,36 @@ import { StButton, StButtonWrap } from 'components/Button';
 import { StTodoBoard } from 'components/Todo/TodoList';
 import { useNavigate } from 'react-router-dom';
 import useInput from 'hooks/useInput';
+import { checkUsers, loginUser } from 'api/users';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 
 function Login() {
+    const { isLoading, isError, data } = useQuery('users', checkUsers);
+    console.log(data);
     const [id, onChangeIdHandler] = useInput('');
     const [pw, onChangePwHandler] = useInput('');
     const navigate = useNavigate();
+
+    //리액트 쿼리
+    const queryClient = useQueryClient();
+    const mutationLogin = useMutation(loginUser, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('users');
+        }
+    });
+
+    const loginHandler = (e) => {
+        e.preventDefault();
+        const loginUserInfo = data.find((item) => item.uid === id && item.pw === pw);
+        console.log(loginUserInfo);
+        const loginInfo = {
+            ...loginUserInfo,
+            isLogin: true
+        };
+        mutationLogin.mutate(loginInfo);
+        navigate('/');
+    };
+
     return (
         <StBg>
             <Header />
@@ -21,10 +46,10 @@ function Login() {
                     <StCloseBtn>
                         <StModalName>로그인</StModalName>
                     </StCloseBtn>
-                    <form>
+                    <form onSubmit={loginHandler}>
                         <StLabel htmlFor="">아이디</StLabel>
                         <StInputForm placeholder="아이디를 입력하세요" name="" type="text" value={id} onChange={onChangeIdHandler} />
-                        {id.length < 5 ? (
+                        {id.length < 2 ? (
                             <StPtag>5자 이상 입력해주세요</StPtag>
                         ) : (
                             <StPtag>
@@ -33,7 +58,7 @@ function Login() {
                         )}
                         <StLabel htmlFor="contents">비밀번호</StLabel>
                         <StInputForm placeholder="비밀번호를 입력하세요" name="" type="password" value={pw} onChange={onChangePwHandler} />
-                        {pw.length < 5 ? (
+                        {pw.length < 2 ? (
                             <StPtag>5자 이상 입력해주세요</StPtag>
                         ) : (
                             <StPtag>
@@ -50,7 +75,7 @@ function Login() {
                                 $fontColor={'black'}>
                                 홈으로가기
                             </StButton>
-                            <StButton disabled={id.length >= 5 && pw.length >= 5 ? false : true} type="submit" $fontColor={'black'}>
+                            <StButton disabled={id.length >= 2 && pw.length >= 2 ? false : true} type="submit" $fontColor={'black'}>
                                 로그인
                             </StButton>
                         </StButtonWrap>
